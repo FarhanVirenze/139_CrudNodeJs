0b03cef80fc39fca75f330ba282bd7c2f73c5763
farhan <farhanvirenze18@gmail.com>
2024-11-14 09:03:57 +0700

    Pertemuan_6

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const todoRouter = require('./routes/tododb.js'); // Importing the todo router
 require('dotenv').config();
 const port = process.env.PORT;
 
+const expressLayout = require('express-ejs-layouts');
+
+const db = require('./database/db');
+
+app.use(expressLayout);
+
 app.use(express.json());
 
 // Set the view engine to EJS
@@ -21,12 +27,26 @@ app.use('/todo', todoRouter);
 
 // Route for the home page
 app.get('/', (req, res) => {
-    res.render('index'); // Renders the "index.ejs" view
+    res.render('index', {
+        layout: 'layouts/main-layout'
+    }); // Renders the "index.ejs" view
 });
 
 // Route for the contact page
 app.get('/contact', (req, res) => {
-    res.render('contact'); // Renders the "contact.ejs" view
+    res.render('contact', {
+        layout: 'layouts/main-layout'
+    }); // Renders the "contact.ejs" view
+});
+
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
 });
 
 // Handle undefined routes
